a9b34f5a112b2847d90805fa10af4d630b5402f0
MCasari-PMEL <matthew.casari@noaa.gov>
2016-10-04 13:05:10 -0700

    Major structure completed, now offload SubSystems

--- Python/Generator/FS.py
+++ Python/Generator/FS.py
@@ -1,6 +1,13 @@
 from tkinter import *
 
-
+class Grid:
+    def __init__(self):
+        self.val = 0
+    def add(self,num=0):
+        self.val += (1+num)
+        return self.val
+    def current(self):
+        return self.val
 
 #Electrical Subclass
 class Elec:
@@ -16,6 +23,7 @@ class Elec:
         self.schem_path = IntVar()
         self.schem_template = IntVar()
         self.firm = Firm()
+        self.photos = IntVar()
     def toggle(self):
         #self.top.set(self.wd.get()^1)
         self.wd_template.set(self.top.get())
@@ -27,6 +35,7 @@ class Elec:
         self.schem_path.set(self.top.get())
         self.schem_template.set(self.top.get())
         self.interfacedoc.set(self.top.get())
+        self.photos.set(self.top.get())
         self.firm.top.set(self.top.get())
         self.firm.toggle()
     def toggle_schem(self):
@@ -63,7 +72,7 @@ class Mech:
     def toggle(self):
         self.drawings.set(self.top.get())
     def toggle_drawings(self):
-        print("toggle drawings")
+        return
 
 #Mooring Subclass
 class Moor:
@@ -75,9 +84,9 @@ class Moor:
         self.bath.set(self.top.get())
         self.diag.set(self.top.get())
     def toggle_bath(self):
-        print("toggle bath")
+        return
     def toggle_diag(self):
-        print("toggle diagrams")
+        return
 
 #Test Subclass
 class FTest:
@@ -89,7 +98,22 @@ class FTest:
         self.data.set(self.top.get())
         self.report.set(self.top.get())
     def toggle_data(self):
-        print("Add toggle data")
+        return
+
+class Photos:
+    def __init__(self):
+        self.top = IntVar()
+    def toggle(self):
+        return
+
+class Cruise:
+    def __init__(self):
+        self.top = IntVar()
+        self.report = IntVar()
+        self.data = IntVar()
+    def toggle(self):
+        self.report.set(self.top.get())
+        self.data.set(self.top.get())
 
 #Subsystem Subclass
 class SubSys:
@@ -111,18 +135,26 @@ class SubSys:
 #Main File Structure
 class FileStructure:
     def __init__(self):
+        self.grid = Grid()
+        
         self.master = Tk()
         self.master.wm_title("Include the following")
         self.master.resizable(width=False,height=True)
-        self.master.geometry('{}x{}'.format(600,1100))
+        #self.master.geometry('{}x{}'.format(600,1100))
+        #self.master.geometry("550x800+30+30")
+    
+        #self.scrollbar = Scrollbar(self.master,side=RIGHT)
+        #self.scrollbar.pack(side=RIGHT,fill=Y)
+        
+        self.header()
+        self.split()
+        
         #Add the names in the order you wish 
         
         #Mechanical
         #self.split()
         self.mech = Mech()
         self.addMechanical(self.mech,0)
-##        self.mech.top = self.addPathWithCommand('Mechanical',0,self.mech.toggle)
-##        self.mech.drawings = self.addPathWithCommand('Drawings',1,self.mech.toggle_drawings)
 
 
         #Electrical
@@ -135,7 +167,13 @@ class FileStructure:
         self.moor = Moor()
         self.addMooring(self.moor,0)
 
+        #Photos
+        self.photos = Photos()
+        self.addPhotos(self.photos,0)
 
+        #Cruise
+        self.cruise = Cruise()
+        self.addCruise(self.cruise,0)
         
         #Test
         #self.split()
@@ -147,14 +185,19 @@ class FileStructure:
         #Subsystem
         #Add Subsystem
         self.split()
+
+        
         self.subsystem = self.addPath("SubSystems",0)
         #self.sub1 = SubSys()
         self.numsub_val = self.addNumberOfSubsystems(1)
         self.sub1 = self.addSubsystem(1)
 
-
-
-        self.qButton = Button(self.master,text="Finished",command=self.quit).grid(sticky=E+S,pady=4)  
+        print(self.grid.current())
+        self.grid.add(10)
+        print(self.grid.current())
+        #self.grid.add()
+        self.qButton = Button(self.master,text="Finished",command=self.quit).grid(row=self.grid.current(),column=3)
+        #,sticky=E+S,pady=4
     def SetInt(self):
         #mechanical systems
         self.mech.set(1)
@@ -162,38 +205,28 @@ class FileStructure:
         #electrical systems
         self.elec.set(1)
         self.elec_wd.set(1)
-    def addMechanical(self,path,level):
-        path.top = self.addPathWithCommand('Mechanical',level,path.toggle)
-        path.drawings = self.addPathWithCommand('Drawings',level+1,path.toggle_drawings)
+
                                
     def get(self):
         self.Mechanical = self.mech.top.get()
         self.Electrical = self.elec.top.get()
-    def addMooring(self,path,level):
-        path.top = self.addPathWithCommand("Mooring",level,path.toggle)
-        path.bath = self.addPathWithCommand("Bathymetry",level+1,path.toggle_bath)
-        path.diag = self.addPathWithCommand("Diagrams",level+1,path.toggle_diag)
 
-    def addTest(self,path,level):
-        path.top = self.addPathWithCommand("Test",level,path.toggle)
-        path.report = self.addFile("Test Report",level + 1)
-        path.data = self.addPath("Data",level+1)
     def addPath(self,name,level=0):
         tempVar = IntVar()
         tempVar.set(1)
-        Checkbutton(self.master,text=(name+'/'),variable=tempVar,justify=RIGHT,font="Helvetica 9 italic",fg="green").grid(column=level,sticky="W")
+        Checkbutton(self.master,text=(name+'/'),variable=tempVar,justify=RIGHT,font="Helvetica 9 italic",fg="green").grid(row=self.grid.add(),column=level,sticky="W")
         return tempVar;
 
     def addPathWithCommand(self,name,level,cmd):
         tempVar = IntVar()
         tempVar.set(1)
-        Checkbutton(self.master,text=(name+'/'),variable=tempVar,command=cmd,justify=RIGHT,font="Helvetica 9 italic",fg="green").grid(column=level,sticky="W")
+        Checkbutton(self.master,text=(name+'/'),variable=tempVar,command=cmd,justify=RIGHT,font="Helvetica 9 italic",fg="green").grid(row=(self.grid.add()),column=level,sticky="W")
         return tempVar;
 
     def addFile(self,name,level=0):
         tempVar = IntVar()
         tempVar.set(1)
-        Checkbutton(self.master,text=(name),variable=tempVar,justify=RIGHT,font="Helvetica 8 bold",fg="blue").grid(column=level,sticky="w")
+        Checkbutton(self.master,text=(name),variable=tempVar,justify=RIGHT,font="Helvetica 8 bold",fg="blue").grid(row=(self.grid.add()),column=level,sticky="w")
         return tempVar
     def addNumberOfSubsystems(self,level):
         val = Entry(self.master)
@@ -207,19 +240,17 @@ class FileStructure:
         return name
     def addSubsystem(self,num):
         temp = SubSys()
-        #temp.top = self.addPathWithCommand("Subsystem #"+str(num),1,temp.toggle)
-        #temp.name_entry = self.addSubsytemName(1)
-##        temp.elec.top = self.addPathWithCommand("Electrical",2,self.elec.toggle)
-##        temp.elec.bom = self.addFile('BOM.csv',3)
-##        temp.elec.userman = self.addFile('UserManual',3)
-##        temp.elec.schem_path = self.addPath('Schematics',3)
-##        temp.elec.schem_template = self.addFile('Template',4)
-        self.addElectrical(temp.elec,1) 
-
+        #self.addElectrical(temp.elec,1)
+        #self.addMechanical(temp.mech,1)
         return temp
+    
     def addNewSubsystem(self):
         temp = 0
-
+        
+    def addMechanical(self,path,level):
+        path.top = self.addPathWithCommand('Mechanical',level,path.toggle)
+        path.drawings = self.addPathWithCommand('Drawings',level+1,path.toggle_drawings)
+        
     def addElectrical(self,path,level):
         path.top = self.addPathWithCommand("Electrical",level,path.toggle)
         path.bom = self.addFile("BOM.csv",level+1)
@@ -239,16 +270,38 @@ class FileStructure:
         path.firm.source_h = self.addFile("source.h",level+3)
 
         path.firm.binaries = self.addPath("Binaries",level+2)
-##        path.top = IntVar()
-##        path.binaries = IntVar()
-##        path.api = IntVar()
-##        path.readme = IntVar()
-##        path.source = IntVar()
-##        path.source_c = IntVar()
-##        path.source_h = IntVar()
-    def split(self,level=0):
-        temp = Label(self.master,text=('______________')).grid(column=0)
 
+    def addMooring(self,path,level):
+        path.top = self.addPathWithCommand("Mooring",level,path.toggle)
+        path.bath = self.addPathWithCommand("Bathymetry",level+1,path.toggle_bath)
+        path.diag = self.addPathWithCommand("Diagrams",level+1,path.toggle_diag)
+
+    def addPhotos(self,path,level):
+        path.top = self.addPathWithCommand("Photos",level,path.toggle)
+
+    def addCruise(self,path,level):
+        path.top = self.addPathWithCommand("Cruise",level,path.toggle)
+        path.report = self.addFile("Cruise Report",level+1)
+        path.data = self.addPath("Data",level+1)
+        
+    def addTest(self,path,level):
+        path.top = self.addPathWithCommand("Test",level,path.toggle)
+        path.report = self.addFile("Test Report",level + 1)
+        path.data = self.addPath("Data",level+1)
+        
+    def split(self,level=0):
+        
+        temp = Label(self.master,text=('__________________________')).grid(row = self.grid.add(),column=0)
+        temp = Label(self.master,text=('__________________________')).grid(row = self.grid.current(),column=1)
+        temp = Label(self.master,text=('__________________________')).grid(row = self.grid.current(),column=2)
+        temp = Label(self.master,text=('__________________________')).grid(row = self.grid.current(),column=3)
+        #temp = Label(self.master,text=('__________________________')).grid(row = self.grid.current(),column=4)
+
+    def header(self):
+        temp = Label(self.master,text=('Top Level'),font="Calibri 11 bold").grid(row=self.grid.current(),column=0)
+        temp = Label(self.master,text=('Subfolder Level 1'),font="Calibri 11 bold").grid(row=self.grid.current(),column=1)
+        temp = Label(self.master,text=('Subfolder Level 3'),font="Calibri 11 bold").grid(row=self.grid.current(),column=2)
+        temp = Label(self.master,text=('Subfolder Level 4'),font="Calibri 11 bold").grid(row=self.grid.current(),column=3)
     def quit(self):
         #self.sub1.name = self.sub1.name_entry.get()
         try:
