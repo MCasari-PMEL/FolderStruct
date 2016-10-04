d94338ecae11159cd78ed38b716b56291cc0deb1
MCasari-PMEL <matthew.casari@noaa.gov>
2016-10-04 14:43:14 -0700

    Added documentation to classes

--- Python/Generator/SubSys.py
+++ Python/Generator/SubSys.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from FS_classes import *
 
 class Grid:
     def __init__(self):
@@ -11,44 +12,75 @@ class Grid:
 
 #Subsystem Subclass
 class SubSystem:
-    def __init__(self,numSubs=1):
+    def __init__(self,root,numsubs):
         self.grid = Grid()
-        self.master = Tk()
-        self.top = IntVar()
-        self.name_entry[numSubs] = {}
-        for i in range(0,numSubs):
-            self.name_entry[i] = self.addSubsytemName(0)
-            self.name[i] = 0
-            
-            self.elec[i] = Elec()
-            self.mech[i] = Mech()
-            self.bom[i] = IntVar()
-            self.userman[i] = IntVar()
+        
+        self.master = root
 
-            self.addElectrical(self.elec,1)
-            self.addMechanical(self.mech,1)
+        self.header1()
+        self.split()
+        #self.top = IntVar()
+        self.name_entry = []
+        self.name = []
+        #
+        self.numsubs = numsubs
+        for i in range(0,numsubs):
+            Label(self.master,text="Subsystem #" + str(i+1)).grid(row=self.grid.add())
+            self.name_entry.append(self.addSubsytemName(1,self.grid.current()))
+            self.name.append(0)
+            self.grid.add()
+        #self.name_entry = self.addSubsytemName(0)
 
-    def toggle(self):
-        self.elec.toggle()
-        self.mech.toggle()
-        self.bom.set(self.top.get())
-        self.userman.set(self.top.get())
-    def addSubsytemName(self,level):
+
+        self.split()
+        self.header2()
+        self.split()
+        #self.name = 0
+
+        #self.top = self.addPathWithCommand("Subsystem",0,self.toggle)
+        
+        self.userman = self.addFile("User Manual",0)
+        self.bom = self.addFile("BOM.csv",0)
+        
+        self.elec = Elec()
+        self.addElectrical(self.elec,0)
+        
+        self.mech = Mech()
+        self.addMechanical(self.mech,0)
+
+        self.photos = Photos()
+        self.addPhotos(self.photos,0)
+
+        self.docu = Docu()
+        self.addDocumentation(self.docu,0)
+        self.split()
+        self.grid.add(5)
+        self.qButton = Button(self.master,text="Finished",command=self.quit,font="Helvetica 12 bold",bg="green").grid(row=self.grid.add(),column=3)
+
+        
+    def toggle(self,num):
+        self.elec[num].toggle()
+        self.mech[num].toggle()
+        self.bom[num].set(self.top.get())
+        self.userman[num].set(self.top.get())
+    def addSubsytemName(self,level,rval):
         name = Entry(self.master)
         name.insert(10,"SubSys Name")
-        name.grid(column=level)
+        name.grid(row=rval,column=level)
         return name
-
+    
     def addElectrical(self,path,level):
         path.top = self.addPathWithCommand("Electrical",level,path.toggle)
         path.bom = self.addFile("BOM.csv",level+1)
         path.powerbudget = self.addFile("PowerBudget.csv",level+1)
         path.interfacedoc = self.addFile("InterfaceDocument",level+1)
         path.userman = self.addFile("User Manual",level+1)
+        path.datasheets = self.addPath("Data Sheets",level+1)
         path.wd = self.addPathWithCommand("Wire Diagrams",level+1,path.toggle_wd)
         path.wd_template = self.addFile("Template",level+2)
         path.schem_path = self.addPathWithCommand("Schematics",level+1,path.toggle_schem)
         path.schem_template = self.addFile("Template",level+2)
+        
         path.firm.top = self.addPathWithCommand("Firmware",level+1,path.firm.toggle)
         
         path.firm.api = self.addFile("API Document",level+2)
@@ -62,6 +94,12 @@ class SubSystem:
     def addMechanical(self,path,level):
         path.top = self.addPathWithCommand('Mechanical',level,path.toggle)
         path.drawings = self.addPathWithCommand('Drawings',level+1,path.toggle_drawings)
+
+    def addPhotos(self,path,level):
+        path.top = self.addPathWithCommand('Photos',level,path.toggle)
+
+    def addDocumentation(self,path,level):
+        path.top = self.addPathWithCommand('Documentation',level,path.toggle)
         
     def addPath(self,name,level=0):
         tempVar = IntVar()
@@ -80,9 +118,48 @@ class SubSystem:
         tempVar.set(1)
         Checkbutton(self.master,text=(name),variable=tempVar,justify=RIGHT,font="Helvetica 8 bold",fg="blue").grid(row=(self.grid.add()),column=level,sticky="w")
         return tempVar
+    def split(self,level=0):
+        
+        temp = Label(self.master,text=('__________________________')).grid(row = self.grid.add(),column=0)
+        temp = Label(self.master,text=('__________________________')).grid(row = self.grid.current(),column=1)
+        temp = Label(self.master,text=('__________________________')).grid(row = self.grid.current(),column=2)
+        temp = Label(self.master,text=('__________________________')).grid(row = self.grid.current(),column=3)
+        self.grid.add()
+    def toggle(self):
+        self.elec.top.set(self.top.get())
+        self.elec.toggle()
+        self.mech.top.set(self.top.get())
+        self.mech.toggle()
+        self.bom.set(self.top.get())
+        self.userman.set(self.top.get())
+
+    def header1(self):
+        Label(self.master,text='Subsystem #',font="Calibri 11 bold").grid(row=self.grid.current(),column=0)
+        Label(self.master,text='Subsytem Name',font="Calibri 11 bold").grid(row=self.grid.current(),column=1)
+        self.grid.add()
+        
+    def header2(self):
+        temp = Label(self.master,text=('Top Level'),font="Calibri 11 bold").grid(row=self.grid.current(),column=0)
+        temp = Label(self.master,text=('Subfolder Level 1'),font="Calibri 11 bold").grid(row=self.grid.current(),column=1)
+        temp = Label(self.master,text=('Subfolder Level 3'),font="Calibri 11 bold").grid(row=self.grid.current(),column=2)
+        temp = Label(self.master,text=('Subfolder Level 4'),font="Calibri 11 bold").grid(row=self.grid.current(),column=3)
+    def quit(self):
+
+        try:
+
+            for i in range(0,self.numsubs):
+                self.name[i] = self.name_entry[i].get()
+                print(self.name[i])
+        except:
+            print("Invalid")
+            
+     
+        self.master.quit
+        self.master.destroy()
 
 
+master = Tk()
+ss1 = SubSystem(master,4)
 
-ss = SubSystem(2)
 
 mainloop()
